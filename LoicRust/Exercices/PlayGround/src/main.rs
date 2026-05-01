e73e58e7430f0855a074199e8419fb3bf91a8115
godef <g0def@proton.me>
2026-05-01 11:41:01 +0200

    feat(WatherStation): add struct exercice¨

--- LoicRust/Exercices/PlayGround/src/main.rs
+++ LoicRust/Exercices/PlayGround/src/main.rs
@@ -1,12 +1,33 @@
-fn hello_world()-> String {
-    println!("Hello world!");
-    String::from("Hello, world!")
+struct WeatherStation {
+    temperature_c: f64,
+    humidity: f64,
+    wind_speed: f64
 }
 
+impl WeatherStation {
+    fn comfort_index(&self) -> f64 {
+        (100.0 - self.humidity) - self.temperature_c - 22.0
+    }
 
+    fn is_stormy(&self) -> bool {
+        self.wind_speed > 80.0
+    }
+}
 
 fn main() {
-    hello_world();
+    let station = WeatherStation{
+        wind_speed: 2.1231,
+        temperature_c: 2.4523,
+        humidity: 12.453
+    };
+
+    if station.is_stormy() {
+        println!("Stormy");
+    }
+    
+    let index = station.comfort_index();
+    
+    dbg!(index);
 }
 
 // --------------------------------------------------------------------------------
@@ -15,9 +36,37 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    
+    #[test]
+    fn is_stormy_should_return_false() {
+        let mock = WeatherStation {
+            wind_speed: 10.0,
+            temperature_c: 0.0,
+            humidity: 0.0
+        };
+
+        assert!(!mock.is_stormy())
+    }
 
     #[test]
-    fn hello_world_should_return_hello_world() {
-        assert_eq!(hello_world(), "Hello, world!");
+    fn is_stormy_should_return_true() {
+        let mock = WeatherStation {
+            wind_speed: 90.0,
+            temperature_c: 0.0,
+            humidity: 0.0
+        };
+
+        assert!(mock.is_stormy())
+    }
+
+    #[test]
+    fn comfort_index_should_return_correct_index() {
+        let mock = WeatherStation{
+            wind_speed: 10.0,
+            temperature_c: 20.0,
+            humidity: 10.0
+        };
+
+        assert_eq!(mock.comfort_index(), 48.0);
     }
 }
